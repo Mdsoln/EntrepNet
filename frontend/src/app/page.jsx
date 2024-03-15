29a64fa01c055331891b89bacf3407ce41cb8eec
loussosthenes9 <loussosthenes9@gmail.com>
2024-03-15 13:09:50 +0300

    fronted added

---
+++ frontend/src/app/page.jsx
@@ -0,0 +1,19 @@
+
+import ImageGrid from "../components/ImageGrid";
+import MaxWidthWrapper from "../components/MaxwidthWrapper";
+import React from "react";
+
+import Hero from "@/components/Hero";
+
+export default function page() {
+  return (
+    <>
+      <MaxWidthWrapper>
+        <div className="flex gap-x-24">
+          <Hero />
+          <ImageGrid />
+        </div>
+      </MaxWidthWrapper>
+    </>
+  );
+}
