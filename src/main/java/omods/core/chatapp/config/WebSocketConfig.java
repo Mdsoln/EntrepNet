06dfed43fab5b4dc2f40ed7561166b166cc3dfe8
Mdsoln <muddyfakih98@gmail.com>
2024-04-08 23:24:23 +0300

    subProtocolWebSocket commenting

--- src/main/java/omods/core/chatapp/config/WebSocketConfig.java
+++ src/main/java/omods/core/chatapp/config/WebSocketConfig.java
@@ -3,9 +3,9 @@ package omods.core.chatapp.config;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
-
+/*
 @Configuration
 @EnableWebSocketMessageBroker
 public class WebSocketConfig {
 
-}
+}*/
