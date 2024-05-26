a6605fd610ed5071b1a26c9c780283ae9388af50
Louissosthenes9 <louissosthenes9@gmail.com>
2024-05-26 19:34:20 +0300

    auth context

---
+++ frontend/src/context/AuthContext.js
@@ -0,0 +1,20 @@
+"use client"
+import {createContext, useContext, useState} from 'react';
+
+// eslint-disable-next-line react/prop-types
+export const AuthContext = createContext();
+// eslint-disable-next-line react-refresh/only-export-components
+export  const useAuthContext =()=>{
+    return useContext(AuthContext)
+}
+
+
+
+
+export const AuthContextProvider = ({ children }) => {
+    const [auth, setAuth] = useState(JSON.parse(localStorage.getItem('jwtToken') )|| null);
+ 
+ return <AuthContext.Provider value={{auth,setAuth }}>
+        {children}
+    </AuthContext.Provider>;
+}
