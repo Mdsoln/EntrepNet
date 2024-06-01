11a7bda5cf2423e5d1f03c7cf600affecf030652
Louissosthenes9 <louissosthenes9@gmail.com>
2024-06-02 00:54:48 +0300

    fixed the layout

--- frontend/src/components/Sidebar.jsx
+++ frontend/src/components/Sidebar.jsx
@@ -2,7 +2,6 @@
 import React from 'react'
 import Header from './Header'
 import { Separator } from './ui/separator'
-import { BsBookmarks } from "react-icons/bs";
 import { BiHelpCircle } from "react-icons/bi";
 import { HiOutlineSpeakerphone } from "react-icons/hi";
 import { Settings,LogOut,Pencil, Mails, CircleHelp} from 'lucide-react';
@@ -15,15 +14,18 @@ export default function Sidebar() {
 
     const handleSignOut = async () => {
         await signOut();
-        // Optionally, you can perform additional actions after signing out
+        
     };
   return (
     <div className='flex flex-col gap-y-12 w-48 mx-0 bg-[#15264B] h-full'>
         <Header fontSize={"text-lg"}/>
         <div><Separator orientation='horizontal' className="w-full" /></div> 
         <div className='flex flex-col gap-y-8 pl-6 '>
-             <div className='text-white hover:rounded-lg flex gap-x-4 hover:bg-teal-50 hover:text-black hover:p-4 transition-all ease'><Mails  /><div>Inbox</div></div>
-             <div className='text-white hover:rounded-lg flex gap-x-4  hover:bg-teal-50 hover:text-black hover:p-4 transition-all ease'><BsBookmarks className='text-lg font-bold'/> <div>Bookmarks</div></div>
+             <div
+              className='text-white hover:rounded-lg flex gap-x-4 hover:bg-teal-50 hover:text-black hover:p-4 transition-all ease'>
+                <Mails  /><Link href={"/inbox"}>Inbox</Link>  
+            </div>
+             {/* <div className='text-white hover:rounded-lg flex gap-x-4  hover:bg-teal-50 hover:text-black hover:p-4 transition-all ease'><BsBookmarks className='text-lg font-bold'/> <div>Bookmarks</div></div> */}
                   <PostModal />
              <div className='text-white hover:rounded-lg flex gap-x-4  hover:bg-teal-50 hover:text-black hover:p-4 transition-all ease'><Settings /> <div>Settings</div></div>
              <div className="text-white flex gap-x-4 hover:rounded-lg hover:bg-teal-50 hover:text-black hover:p-4 transition-all ease"><HiOutlineSpeakerphone className='text-2xl font-extrabold'/>
@@ -37,7 +39,7 @@ export default function Sidebar() {
             </div>
              <div className='text-white flex gap-x-4  hover:bg-teal-50 hover:text-black hover:p-4 transition-all ease' onClick={handleSignOut}> <LogOut/><div>logout</div></div>
         </div>
-       
+  
     </div>
   )
 }
