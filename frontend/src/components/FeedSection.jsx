7638c022aa6b4ba3587f5c6ff2da48238da165cb
Louissosthenes9 <louissosthenes9@gmail.com>
2024-06-11 19:38:25 +0300

    added post

--- frontend/src/components/FeedSection.jsx
+++ frontend/src/components/FeedSection.jsx
@@ -1,9 +1,40 @@
-import React from 'react'
+import axios from 'axios'
+import React, { useEffect, useState } from 'react'
 
 export default function FeedSection() {
+  const [posts,setPosts] = useState([])
+  const [loading,setLoading] = useState(false)
+
+  useEffect(
+  ()=>{
+ 
+        const fetchPost =  async ()=>{
+          setLoading(true)
+         try {
+          let res = await axios.get('http://localhost:8080/api/v1/post/recentPosts')
+          let data = res.data
+          setPosts(data)
+          console.log(data)
+         } catch (error) {
+          console.log(error)
+         }finally{
+            setLoading(false)
+         }
+      }
+
+      fetchPost()
+  },
+  []
+  )
   return (
-    <div>
-        hhhh
-    </div>
+    loading?(
+      <p>Loading...</p>
+    ):(
+      posts.map((post,i)=>(
+        <div key={i}>
+            {post}
+        </div>
+      ))
+    )
   )
 }
