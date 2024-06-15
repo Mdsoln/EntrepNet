c1c9d0ff0ccf9ff7b4ea1d1d8024b8e3756ca5f1
Mdsoln <muddyfakih98@gmail.com>
2024-06-15 17:11:03 +0300

    permitting all websocket endpoints

--- src/main/java/omods/core/jwt/config/Security.java
+++ src/main/java/omods/core/jwt/config/Security.java
@@ -26,6 +26,10 @@ public class Security {
                         auth -> auth
                                 .requestMatchers("/api/v1/post/**","/api/v1/user/**","/images/{imageName}")
                                 .permitAll()
+                                .requestMatchers("/ws","/users","/app")
+                                .permitAll()
+                                .requestMatchers("/chat","/message/{senderId}/{receiverId}","/users.disconnectUser","/users/topic","/users","/users.addUser")
+                                .permitAll()
                                 .requestMatchers("/api/v1/admin/**").hasRole("ADMIN")
                                 .requestMatchers("/api/v1/entrepreneur/**").hasAnyRole("ENTREPRENEUR","ADMIN")
                                 .anyRequest()
