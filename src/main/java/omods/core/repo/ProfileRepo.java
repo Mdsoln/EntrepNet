c188aa0d69777d7bd43c6a44d1ab732b78e454c5
Mdsoln <muddyfakih98@gmail.com>
2024-06-17 08:17:06 +0300

    associate user image and job with token as extra claims

--- src/main/java/omods/core/repo/ProfileRepo.java
+++ src/main/java/omods/core/repo/ProfileRepo.java
@@ -3,11 +3,16 @@ package omods.core.repo;
 import omods.core.entity.Profile;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
+
 
 @Repository
 public interface ProfileRepo extends JpaRepository<Profile, Long> {
 
-    @Query("SELECT p.job FROM Profile p")
-    Object findJob();
+    @Query("SELECT p.job FROM Profile p WHERE p.user.id = :userId")
+    String findJobByUserId(@Param("userId") Long userId);
+
+    @Query("SELECT p.imagePath FROM Profile p WHERE p.user.id = :userId")
+    String findImagesByUserId(@Param("userId") Long userId);
 }
